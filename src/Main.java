b5d33bb2d38fb79433ce63cb62eb62186a1926c0
Isaias Brasil <isaiasbrasil7@gmail.com>
2023-03-27 23:03:51 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int age = 10;
+		int x = 5;
+		int y = 7;
+		int resultado = x * y;
+		System.out.println("Exemplo 1-> O valor de x*y é: " + resultado);
+		
+		double salario = 1200.0;
+		System.out.println("Exemplo 2-> O salario é:" + salario);
+		float preco = 22.0f;
+		System.out.println("Exemplo 3-> O preço de x*y " + preco);
+		
+		String nome = "Maria de Souza";
+		System.out.println("Exemplo 4-> O nome é: " + nome);
+		
+		char id = 's';
+		System.out.println("Exemplo 5-> A identificação desse caracter é " + id);
+		
+		System.out.println("O dela é: " + nome +". " + nome + " tem " + age + " anos");
+	}
+}
